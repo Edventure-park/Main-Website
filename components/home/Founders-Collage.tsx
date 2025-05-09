9f7ae973f3e30f1c660f7893b82303d7467aae17
Adnan <adnanali23189@gmail.com>
2025-05-09 09:53:13 +0530

    feat: Add Founder's Image Component

--- components/home/Founders-Collage.tsx
+++ components/home/Founders-Collage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-function Founders_Collage() {
+export default function Founders_Collage() {
   return (
-    <div className='text-center'>
-      Founder's Collage component in here
-    </div>
-  )
+    <section className="w-full bg-white px-4 py-8">
+      <div className="mx-auto max-w-7xl">
+        <div className="relative w-full overflow-hidden rounded-lg shadow-lg">
+          <Image
+            src="/assets/founder_collage.jpg" // Ensure this path matches your public folder structure
+            alt="Founders at Edventure Park"
+            width={2048}
+            height={767}
+            layout="responsive"
+            objectFit="contain"
+            priority
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
-
-export default Founders_Collage
