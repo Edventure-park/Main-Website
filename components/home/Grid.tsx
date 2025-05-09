b390da86665ab0490d826ed859207adad81d05ed
Adnan <adnanali23189@gmail.com>
2025-05-09 08:50:59 +0530

    feat: Add Grid row 4

--- components/home/Grid.tsx
+++ components/home/Grid.tsx
@@ -28,7 +28,7 @@ function Grid() {
           </div>
         </div>
       </div>
-      {/* Row 2 - Original 8 Column Grid */}
+      {/* Row 2 - First 8 Column Grid */}
       <div className="grid w-full grid-cols-8">
         <div className="aspect-square bg-green-600"></div>
         <div className="relative aspect-square">
@@ -47,11 +47,11 @@ function Grid() {
         <div className="aspect-square bg-green-600"></div>
         <div className="aspect-square bg-green-600"></div>
       </div>
-      {/* Row 3 - New 8 Column Grid with specified pattern */}
+      {/* Row 3 - Second 8 Column Grid */}
       <div className="grid w-full grid-cols-8">
         {/* Column 1 - Person's Image */}
         <div className="relative aspect-square">
-          <Image alt='arma img' width={500} height={500} className="h-full w-auto object-cover" src='/assets/grid/arma.png' />
+          <Image alt='arma img' fill className="object-cover" src='/assets/grid/arma.png' sizes="(max-width: 768px) 12.5vw, 12.5vw" />
         </div>
         {/* Columns 2-3 - Green Boxes */}
         <div className="aspect-square bg-green-600"></div>
@@ -65,8 +65,34 @@ function Grid() {
         <div className="aspect-square bg-white"></div>
         {/* Column 8 - Person's Image */}
         <div className="relative aspect-square">
-          <Image alt='rahman img' width={500} height={500} className="h-full w-auto object-cover" src='/assets/grid/abdulrahman.png' />
+          <Image alt='rahman img' fill className="object-cover" src='/assets/grid/abdulrahman.png' sizes="(max-width: 768px) 12.5vw, 12.5vw" />
         </div>
+      </div>
+      {/* Row 4 - Third 8 Column Grid */}
+      <div className="grid w-full grid-cols-8">
+        {/* Column 1 - Green Box */}
+        <div className="aspect-square bg-green-600"></div>
+        {/* Column 2 - Person's Image */}
+        <div className="relative aspect-square">
+          <Image alt='fouzan img' width={500} height={500} className="h-full w-auto object-cover" src='/assets/grid/fouzan.png' />
+        </div>
+        {/* Column 3 - Blank Box */}
+        <div className="aspect-square bg-white"></div>
+        {/* Columns 4-5 - Person's Images */}
+        <div className="relative aspect-square">
+          <Image alt='grid img' width={500} height={500} className="h-full w-auto object-cover" src='/assets/grid/ba1.png' />
+        </div>
+        <div className="relative aspect-square">
+          <Image alt='grid img' width={500} height={500} className="h-full w-auto object-cover" src='/assets/grid/ba2.png' />
+        </div>
+        {/* Column 6 - Blank Box */}
+        <div className="aspect-square bg-white"></div>
+        {/* Column 7 - Person's Image */}
+        <div className="relative aspect-square">
+          <Image alt='nisa img' width={500} height={500} className="h-full w-auto object-cover" src='/assets/grid/nisa.png' />
+        </div>
+        {/* Column 8 - Blank Box */}
+        <div className="aspect-square bg-white"></div>
       </div>
     </div>
   );
