14d858d584a62e9113f0d8c96a6ae8097f73fc3e
ArshilAliAbbas <arshil1227@gmail.com>
2025-05-09 06:45:46 -0400

    test1

---
+++ app/web/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <h1>Welcome to the Web App</h1>
+      <p>This is a simple web application.</p>
+      <p>It is built using React and TypeScript.</p>
+      <p>Feel free to explore the code!</p>
+      <p>Have a great day!</p>
+      <p>Enjoy coding!</p>
+      <p>Happy learning!</p>
+      <p>Keep pushing your limits!</p>
+      <p>Stay curious!</p>
+      <p>Keep experimenting!</p>
+      <p>Never stop learning!</p>
+    </div>
+  );
+}
+
+export default Page;
