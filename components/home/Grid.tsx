5e79108f080b47bc81f2fe6deb442dbbd3e6ae93
Adnan <adnanali23189@gmail.com>
2025-05-09 08:40:47 +0530

    feat: Add Grid row 3

--- components/home/Grid.tsx
+++ components/home/Grid.tsx
@@ -8,7 +8,6 @@ function Grid() {
   return (
     <div className="flex w-full flex-col">
       {/* Row 1 - Header */}
-      {/* STUDENT Group */}
       <div className="flex w-full flex-nowrap items-center justify-center gap-2 overflow-x-auto whitespace-nowrap text-3xl font-bold tracking-wider sm:gap-4 sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
         {/* STUDENT Group */}
         <div className="flex">
@@ -29,7 +28,7 @@ function Grid() {
           </div>
         </div>
       </div>
-      {/* Row 2 - 8 Column Grid */}
+      {/* Row 2 - Original 8 Column Grid */}
       <div className="grid w-full grid-cols-8">
         <div className="aspect-square bg-green-600"></div>
         <div className="relative aspect-square">
@@ -47,6 +46,27 @@ function Grid() {
         <div className="aspect-square bg-white"></div>
         <div className="aspect-square bg-green-600"></div>
         <div className="aspect-square bg-green-600"></div>
+      </div>
+      {/* Row 3 - New 8 Column Grid with specified pattern */}
+      <div className="grid w-full grid-cols-8">
+        {/* Column 1 - Person's Image */}
+        <div className="relative aspect-square">
+          <Image alt='arma img' width={500} height={500} className="h-full w-auto object-cover" src='/assets/grid/arma.png' />
+        </div>
+        {/* Columns 2-3 - Green Boxes */}
+        <div className="aspect-square bg-green-600"></div>
+        <div className="aspect-square bg-green-600"></div>
+        {/* Columns 4-5 - Blank Boxes */}
+        <div className="aspect-square bg-white"></div>
+        <div className="aspect-square bg-white"></div>
+        {/* Column 6 - Green Box */}
+        <div className="aspect-square bg-green-600"></div>
+        {/* Column 7 - Blank Box */}
+        <div className="aspect-square bg-white"></div>
+        {/* Column 8 - Person's Image */}
+        <div className="relative aspect-square">
+          <Image alt='rahman img' width={500} height={500} className="h-full w-auto object-cover" src='/assets/grid/abdulrahman.png' />
+        </div>
       </div>
     </div>
   );
