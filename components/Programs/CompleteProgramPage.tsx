33f2688c3ef1b8d25c8bd489c1baf6be5df5cf9a
Adnan <adnanali23189@gmail.com>
2025-05-14 18:49:33 +0530

    dynamic program init

---
+++ components/Programs/CompleteProgramPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CompleteProgramPage({slug}:{slug:string}) {
+    if (!slug){
+        return <div>Loading...</div>
+    }
+  
+    return (
+      <div>
+        hmmm
+      </div>
+    )
+}
+
+export default CompleteProgramPage
---
+++ components/Programs/ProgramInfo.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+interface ProgramInfoProps {
+    Heading: string;
+    SubHeading: string;
+    Paragraph: string;
+    lists: string[];
+}
+
+function ProgramInfo({ProgramInfoData}:{ProgramInfoData:ProgramInfoProps}) {
+  return (
+    <div>
+      hmmm
+    </div>
+  )
+}
+
+export default ProgramInfo
