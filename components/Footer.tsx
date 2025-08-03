329cc63d50142beef9fe9fb04f1276c73f84b73f
Syed Adnan Ali <93512252+Adnan-The-Coder@users.noreply.github.com>
2025-08-03 16:38:08 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,33 +11,16 @@ const teamMembers = [
   {
     id: 1,
     name: "Syed Adnan Ali",
-    designation: "Project Lead",
+    designation: "Full Stack Developer",
     image: "/AdnanIm-1.jpg",
   },
   {
-    id: 3,
-    name: "Arjun Reddy",
-    designation: "Technology Lead",
-    image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+    id: 2,
+    name: "Arshil Ali Abbas",
+    designation: "Frontend developer",
+    image: "/Arshil.jpg",
   },
-  {
-    id: 4,
-    name: "Sneha Patel",
-    designation: "Community Manager",
-    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 5,
-    name: "Vikram Singh",
-    designation: "Operations Head",
-    image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-  },
-  {
-    id: 6,
-    name: "Kavya Nair",
-    designation: "Design Lead",
-    image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-  },
+
 ];
 
 export default function Footer() {
