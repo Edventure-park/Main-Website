ba09c0fab2fdeddbbd83abd421c5865e8308446c
Adnan <adnanali23189@gmail.com>
2025-11-06 09:50:19 +0530

    fix: build error

--- components/blogs/Landing.tsx
+++ components/blogs/Landing.tsx
@@ -31,7 +31,7 @@ const Blogs: React.FC = () => {
     const handleResize = () => setIsMobile(window.innerWidth < 768);
     handleResize();
     window.addEventListener("resize", handleResize);
-    
+
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
